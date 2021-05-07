2d9a8f6233b6eed75ed9f9b5d56401a4e7519541
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-07 19:32:34 +0200

    chore: 🛠 use BehaviorSubject

--- src/app/wishlist.component.ts
+++ src/app/wishlist.component.ts
@@ -1,53 +1,37 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Wishlist } from './wishlist';
-import { Book, createBook } from './book';
+import { Book } from './book';
+import { map } from 'rxjs/operators';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-wishlist',
   template: `
     <h1>My Wishlist</h1>
     <ul>
-      <button (click)="addBook()">ADD</button>
-      <mc-book-preview *ngFor="let book of getBooks()" [book]="book">
+      <mc-book-preview *ngFor="let book of books$ | async" [book]="book">
         <button (click)="removeBook(book)">REMOVE</button>
       </mc-book-preview>
     </ul>
     <hr />
-    <div>Total: <mc-price [price]="getTotalPrice()"></mc-price></div>
+    <div>
+      Total ({{ count$ | async }}):
+      <mc-price [price]="totalPrice$ | async"></mc-price>
+    </div>
   `,
 })
 export class WishlistComponent {
-  constructor(private _wishlist: Wishlist) {
-    this._wishlist.addBook(
-      createBook({
-        name: 'eXtreme Programming Explained',
-        price: 30,
-      })
-    );
-    this._wishlist.addBook(
-      createBook({
-        name: 'FullWebDev',
-        price: 45,
-      })
-    );
-  }
+  books$ = this._wishlist.books$;
+  count$ = this.books$.pipe(map((books) => books.length));
+  totalPrice$ = this.books$.pipe(
+    map((books) => {
+      return books.reduce((total, book) => total + (book.price ?? 0), 0);
+    })
+  );
 
-  getBooks() {
-    return this._wishlist.getBookList();
-  }
-
-  getTotalPrice() {
-    return this.getBooks().reduce(
-      (total, book) => total + (book.price ?? 0),
-      0
-    );
-  }
+  constructor(private _wishlist: Wishlist) {}
 
   removeBook(book: Book) {
     this._wishlist.removeBook(book);
-  }
-
-  addBook() {
-    this._wishlist.addBook(createBook({ name: 'test', price: 3 }));
   }
 }
--- src/app/wishlist.ts
+++ src/app/wishlist.ts
@@ -1,35 +1,48 @@
 import { Book } from './book';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Wishlist {
-  private _books: Book[] = [];
+  books$: Observable<Book[]>;
+
+  private _books$ = new BehaviorSubject<Book[]>([]);
+
+  constructor() {
+    this.books$ = this._books$.asObservable();
+  }
 
   addBook(book: Book) {
-    this._books = [...this._books, book];
+    this._setBooks([...this._getBooks(), book]);
   }
 
   removeBook(book: Book) {
-    this._books = this._books.filter((_book) => book !== _book);
-  }
-
-  getBookList(): Book[] {
-    return this._books;
+    this._setBooks(this._getBooks().filter((_book) => book !== _book));
   }
 
   update(book: Book, changes: Partial<Book>) {
-    this._books = this._books.map((_book) => {
-      if (_book === book) {
-        /* This is the new book. */
-        return {
-          ...book,
-          ...changes,
-        };
-      }
+    this._setBooks(
+      this._getBooks().map((_book) => {
+        if (_book === book) {
+          /* This is the new book. */
+          return {
+            ...book,
+            ...changes,
+          };
+        }
 
-      return _book;
-    });
+        return _book;
+      })
+    );
+  }
+
+  private _getBooks(): Book[] {
+    return this._books$.value;
+  }
+
+  private _setBooks(books: Book[]) {
+    this._books$.next(books);
   }
 }
