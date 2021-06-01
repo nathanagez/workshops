bdb00e20c95a799e31c5952aab0c48b6378f2f19
Younes Jaaidi <yjaaidi@gmail.com>
2021-06-01 03:25:03 +0200

    🛠 test with fake repository

--- apps/whiskmate/src/app/recipe/recipe-search.cy-spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.cy-spec.ts
@@ -1,8 +1,34 @@
+import { of } from 'rxjs';
+import { RecipeFilter } from './recipe-filter';
+import { RecipeRepository } from './recipe-repository.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { mount } from '@jscutlery/cypress-angular/mount';
 import { getHarness } from '@jscutlery/cypress-harness';
 import { RecipeSearchComponent } from './recipe-search.component';
 import { RecipeSearchHarness } from './recipe-search.harness';
+
+class FakeRecipeRepository {
+  _recipes = [
+    {
+      id: 'cauliflower-pomegranate-and-pistachio-salad',
+      name: 'Cauliflower, pomegranate and pistachio salad',
+      pictureUri:
+        'https://ottolenghi.co.uk/media/contentmanager/content/cache/646x458//Cauliflower,-pomegranate-and-pistachio-salad.jpg',
+    },
+    {
+      id: 'braised-eggs-with-leek-and-za-atar',
+      name: 'Braised eggs with leek and za’atar',
+      pictureUri:
+        'https://ottolenghi.co.uk/media/contentmanager/content/cache/646x458//Braised-eggs-with-leek-and-za%E2%80%99atar.jpg',
+    },
+  ];
+
+  search(filter: RecipeFilter) {
+    return of(
+      Object.entries(filter).length === 0 ? this._recipes : [this._recipes[0]]
+    );
+  }
+}
 
 describe(RecipeSearchComponent.name, () => {
   const harness = getHarness(RecipeSearchHarness);
@@ -12,6 +38,12 @@ describe(RecipeSearchComponent.name, () => {
       imports: [BrowserAnimationsModule],
       /* eslint-disable-next-line @typescript-eslint/no-var-requires */
       style: require('!!css-loader!../../styles.css').toString(),
+      providers: [
+        {
+          provide: RecipeRepository,
+          useClass: FakeRecipeRepository,
+        },
+      ],
     })
   );
 
@@ -21,7 +53,7 @@ describe(RecipeSearchComponent.name, () => {
       'contain',
       'Cauliflower, pomegranate and pistachio salad'
     );
-    recipeNames.should('have.length', 10);
+    recipeNames.should('have.length', 2);
   });
 
   it('should filter recipes', () => {
