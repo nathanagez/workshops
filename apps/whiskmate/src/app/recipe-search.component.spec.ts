4ea22f378d58ad6a50ac079e3922c3b23eb0181f
Younes Jaaidi <yjaaidi@gmail.com>
2022-04-21 18:12:25 +0200

    test: ✅ add empty specs for <wm-recipe-search>

---
+++ apps/whiskmate/src/app/recipe-search.component.spec.ts
@@ -0,0 +1,11 @@
+describe('RecipeSearchComponent', () => {
+  it.todo('🚧 should display "Loading..."');
+
+  it.todo('🚧 should load recipes on startup');
+
+  it.todo('🚧 should search recipes on filter change');
+
+  it.todo('🚧 should show error message on error');
+
+  it.todo('🚧 should still work after error');
+});
