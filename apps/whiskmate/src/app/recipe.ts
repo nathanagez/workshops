f617ba5bf4219590893d7c83d5a17154577681ef
Younes Jaaidi <yjaaidi@gmail.com>
2022-06-08 12:10:22 +0200

    refactor: 🛠 factorize recipe creation

--- apps/whiskmate/src/app/recipe.ts
+++ apps/whiskmate/src/app/recipe.ts
@@ -1,5 +1,11 @@
 export interface Recipe {
   id: string;
   name: string;
+  pictureUri?: string;
   ingredients?: string[];
+  steps?: string[];
+}
+
+export function createRecipe(recipe: Recipe) {
+  return recipe;
 }
