b4f87eb8b27d9b3e460aedca77e8e5d1d1ef90fe
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-13 15:33:30 +0100

    refactor: 🛠 sfamize <mp-app>

--- apps/meal-planner/src/app/app.component.ts
+++ apps/meal-planner/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'mp-app',
-  template: `<h1>{{ title }}</h1>`
+  template: `<h1>{{ title }}</h1>`,
 })
 export class AppComponent {
   title = 'meal-planner';
 }
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
