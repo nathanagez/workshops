3eab818c9418f2eaffe9012a1fdae112a7bbdff0
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-14 17:55:39 +0100

    refactor: 🛠 use @rx-angular/state to handle local state

--- apps/meal-planner/src/app/recipe-search.component.ts
+++ apps/meal-planner/src/app/recipe-search.component.ts
@@ -1,50 +1,80 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AsyncPipe, NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { RecipeRepository } from './recipe-repository.service';
 import { Recipe } from './recipe';
 import { RecipePreviewComponent } from './recipe-preview.component';
-import {
-  concatMap,
-  exhaustMap,
-  interval,
-  map,
-  mapTo,
-  mergeMap,
-  Observable,
-  retry,
-  switchMap,
-  timer,
-} from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RxState } from '@rx-angular/state';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mp-recipe-search',
   standalone: true,
-  imports: [NgForOf, RecipePreviewComponent, AsyncPipe, ReactiveFormsModule],
+  imports: [
+    NgForOf,
+    RecipePreviewComponent,
+    AsyncPipe,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   template: `
     <form [formGroup]="form">
       <input type="text" formControlName="keywords" />
     </form>
 
-    <mp-recipe-preview
-      *ngFor="let recipe of recipes$ | async"
-      [recipe]="recipe"
-    ></mp-recipe-preview>
+    <ng-container *ngFor="let recipe of recipes$ | async">
+      <mp-recipe-preview [recipe]="recipe"> </mp-recipe-preview>
+      <button (click)="removeRecipe(recipe)">REMOVE</button>
+    </ng-container>
+
+    <div *ngIf="count$ | async as count">Showing {{ count }} results</div>
   `,
+  providers: [RxState],
 })
 export class RecipeSearchComponent {
   form = new FormGroup({
     keywords: new FormControl<string | null>(null),
   });
 
-  recipes$ = this.form.controls.keywords.valueChanges.pipe(
-    switchMap((keywords) =>
-      timer(0, 1000).pipe(
-        exhaustMap(() => this._recipeRepository.getRecipes({ keywords }))
-      )
-    )
-  );
+  state: RxState<State> = inject(RxState);
+  recipes$ = this.state.select('recipes');
+  count$ = this.state.select('recipes', 'length');
 
   private _recipeRepository = inject(RecipeRepository);
+
+  constructor() {
+    this.state.set({
+      keywords: null,
+    });
+
+    /* Fetch recipes each time keywords change. */
+    this.state.connect(
+      'recipes',
+      this.state
+        .select('keywords')
+        .pipe(
+          switchMap((keywords) =>
+            this._recipeRepository.getRecipes({ keywords })
+          )
+        )
+    );
+
+    /* Sync reactive form with state. */
+    this.state.connect(
+      'keywords',
+      this.form.controls.keywords.valueChanges.pipe(debounceTime(50))
+    );
+  }
+
+  removeRecipe(recipe: Recipe) {
+    this.state.set(({ recipes }) => {
+      return { recipes: recipes.filter((r) => r.id !== recipe.id) };
+    });
+  }
+}
+
+interface State {
+  keywords: string | null;
+  recipes: Recipe[];
 }
