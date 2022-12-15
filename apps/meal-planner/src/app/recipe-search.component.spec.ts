627f02aa1a65a4d45e3991779643a0e0265f1734
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-15 17:14:43 +0100

    test: ✅ add Cypress component testing
    nx g @nrwl/angular:cypress-component-configuration --project meal-planner

---
+++ apps/meal-planner/src/app/recipe-search.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { RecipeSearchComponent } from './recipe-search.component';
+
+describe('...', () => {
+  function createComponent() {
+    const fixture = TestBed.createComponent(RecipeSearchComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
+});
