9723646f8692ebefb7d05ffe7260d73d960a00ff
Younes Jaaidi <yjaaidi@gmail.com>
2023-09-13 14:56:23 +0200

    refactor: 🛠 refactor carousel with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RecipeSearchComponent } from './recipe-search.component';
+import { RecipeCarouselComponent } from './recipe-carousel.component';
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
-  imports: [RecipeSearchComponent],
+  imports: [RecipeSearchComponent, RecipeCarouselComponent],
   template: `
-        <app-recipe-search/>
+        <app-recipe-carousel/>
+        <!--<app-recipe-search/>-->
     `,
 })
 export class AppComponent {}
--- src/app/recipe-carousel.component.ts
+++ src/app/recipe-carousel.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from './recipe';
 import { RecipePreviewComponent } from './recipe-preview.component';
 
 @Component({
   selector: 'app-recipe-carousel',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [CommonModule, RecipePreviewComponent],
   template: `
-    <app-recipe-preview [recipe]="recipes[index]"/>
-    <button [disabled]="!hasPrevious()" (click)="goToPrevious()">
-      PREVIOUS
-    </button>
-    <button [disabled]="!hasNext()" (click)="goToNext()">NEXT</button>
-  `,
+        <app-recipe-preview [recipe]="currentRecipe()"/>
+        <button [disabled]="!hasPrevious()" (click)="goToPrevious()">
+            PREVIOUS
+        </button>
+        <button [disabled]="!hasNext()" (click)="goToNext()">NEXT</button>
+    `,
 })
 export class RecipeCarouselComponent {
-  index = 0;
+  index = signal(0);
   recipes: Recipe[] = [
     {
       id: 'burger',
@@ -31,20 +37,21 @@ export class RecipeCarouselComponent {
       name: 'Pasta',
     },
   ];
-
-  hasPrevious() {
-    return this.index > 0;
-  }
-
-  hasNext() {
-    return this.index < this.recipes.length - 1;
-  }
+  currentRecipe = () => this.recipes[this.index()];
+  hasPrevious = () => this.index() > 0;
+  hasNext = () => this.index() < this.recipes.length - 1;
 
   goToNext() {
-    this.index++;
+    setTimeout(
+      () =>
+        this.index.update((index) =>
+          Math.min(index + 1, this.recipes.length - 1)
+        ),
+      500
+    );
   }
 
   goToPrevious() {
-    this.index--;
+    setTimeout(() => this.index.update((index) => Math.max(index - 1, 0)), 500);
   }
 }
