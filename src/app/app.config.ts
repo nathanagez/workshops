a3ef7f60fc53612a53d726e3865106b1f51c0c78
Younes Jaaidi <yjaaidi@gmail.com>
2023-09-13 22:46:59 +0200

    refactor: 🛠 set up @angular/material
    nx add @angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { retryInterceptor } from './utils/retry.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideHttpClient(withInterceptors([retryInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
   ],
