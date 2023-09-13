ab455d522ca72dc9f7eb0a17bb2026d9464b5c57
Younes Jaaidi <yjaaidi@gmail.com>
2023-09-13 12:02:50 +0200

    feat: ✨ optimize reactive search

--- src/app/recipe-search.component.ts
+++ src/app/recipe-search.component.ts
@@ -5,6 +5,16 @@ import { RecipePreviewComponent } from './recipe-preview.component';
 import { RecipeRepository } from './recipe-repository.service';
 import { RecipeFilterComponent } from './recipe-filter.component';
 import { RecipeFilterV2Component } from './recipe-filter-v2.component';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  interval,
+  retry,
+  startWith,
+  Subject,
+  switchMap,
+} from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-recipe-search',
@@ -16,24 +26,32 @@ import { RecipeFilterV2Component } from './recipe-filter-v2.component';
     RecipePreviewComponent,
   ],
   template: `
-<!--        <app-recipe-filter (filterSubmit)="search($event)"/>-->
-        <app-recipe-filter-v2 (filterChange)="search($event)"/>
+<!--        <app-recipe-filter (filterSubmit)="keywords$.next($event)"/>-->
+        <app-recipe-filter-v2 (filterChange)="keywords$.next($event)"/>
         <hr>
         <app-recipe-preview *ngFor="let recipe of recipes" [recipe]="recipe"/>
     `,
 })
 export class RecipeSearchComponent implements OnInit {
+  keywords$ = new Subject<string | undefined>();
   recipes?: Recipe[];
+  recipes$ = this.keywords$.pipe(
+    startWith(undefined),
+    debounceTime(100),
+    distinctUntilChanged(),
+    switchMap((keywords) =>
+      this._repo.searchRecipes(keywords).pipe(
+        retry({
+          delay: (_, retryCount) => interval(retryCount * 1000),
+        })
+      )
+    ),
+    takeUntilDestroyed()
+  );
 
   private _repo = inject(RecipeRepository);
 
   ngOnInit() {
-    this.search();
-  }
-
-  search(keywords?: string) {
-    this._repo
-      .searchRecipes(keywords)
-      .subscribe((recipes) => (this.recipes = recipes));
+    this.recipes$.subscribe((recipes) => (this.recipes = recipes));
   }
 }
