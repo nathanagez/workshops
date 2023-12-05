3c330da825a249e59e0e40bf4bebee65e22df7cd
Younes Jaaidi <yjaaidi@gmail.com>
2023-12-05 16:56:36 +0100

    refactor: 🛠 use axios

--- apps/whiskmate/src/app/greetings/greetings.client.ts
+++ apps/whiskmate/src/app/greetings/greetings.client.ts
@@ -1,14 +1,15 @@
 import { GreetingsResponseDto } from '@whiskmate/shared';
 import { createContext } from 'react';
+import { axiosClient } from '../axios-client';
 
 export interface GreetingsClient {
   getGreetings(): Promise<GreetingsResponseDto>;
 }
 
 class GreetingsClientImpl implements GreetingsClient {
-  async getGreetings(): Promise<GreetingsResponseDto> {
-    const response = await fetch('http://localhost:3333/greetings');
-    return await response.json();
+  async getGreetings() {
+    const { data } = await axiosClient.get<GreetingsResponseDto>('/greetings');
+    return data;
   }
 }
 
