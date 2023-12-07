e4450773ceb333d50cc137629db2e6ad616083ed
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-12-07 16:49:24 +0100

    last

--- apps/recipes-api/src/recipes/recipes.router.ts
+++ apps/recipes-api/src/recipes/recipes.router.ts
@@ -3,13 +3,23 @@ import { inject } from '../di';
 import {RECIPES_REPOSITORY_TOKEN} from "./recipes.repository";
 import {RecipeResponseDto} from "@whiskmate/shared";
 
+import { Recipe } from './recipe';
+
 export const recipesRouter = Router();
 
-recipesRouter.get('/recipes', async (_, res) => {
+const filterRecipes = (recipes: Array<Recipe>, filter: string) => {
+  return recipes.filter((recipe) => {
+    return recipe.name.toLowerCase().includes(filter.toLowerCase());
+  });
+}
+
+recipesRouter.get('/recipes', async (req, res) => {
   const repo = inject(RECIPES_REPOSITORY_TOKEN);
 
+  const filter: string = req.query?.filter as string;
+
   const body: RecipeResponseDto = {
-    items: await repo.getRecipes(),
+    items: filter ? filterRecipes(await repo.getRecipes(), filter) : await repo.getRecipes(),
   };
 
   res.send(body);
--- apps/recipes-api/src/recipes/repices.router.spec.ts
+++ apps/recipes-api/src/recipes/repices.router.spec.ts
@@ -39,6 +39,50 @@ describe(recipesRouter.name, () => {
       })
     );
   });
+
+  it('Filter request should return the searched item', async () => {
+    const { client } = setUp();
+
+    const response = await client.get('/recipes?filter=Basil Pasta');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.items).toContainEqual(
+      expect.objectContaining({
+        "id": "rec_1",
+        "name": "Creamy Tomato Basil Pasta",
+        "description": "A simple and delicious pasta dish with a creamy tomato basil sauce.",
+        "pictureUrl": "https://dishingouthealth.com/wp-content/uploads/2022/09/CreamyTomatoPasta_Square.jpg",
+        "steps": [
+          "Cook the pasta according to package directions.",
+          "While the pasta is cooking, heat the olive oil in a large skillet over medium heat.",
+          "Add the garlic and cook until fragrant, about 30 seconds.",
+          "Add the diced tomatoes, tomato paste, and basil. Bring to a simmer and cook for 10 minutes.",
+          "Stir in the heavy cream and season with salt and pepper to taste.",
+          "Drain the pasta and add it to the skillet with the sauce. Toss to coat.",
+          "Serve immediately with grated Parmesan cheese."
+        ],
+        "ingredients": [
+          "1 pound pasta",
+          "1/4 cup olive oil",
+          "3 cloves garlic, minced",
+          "1 (14.5-ounce) can diced tomatoes, undrained",
+          "2 tablespoons tomato paste",
+          "1/2 cup fresh basil, chopped",
+          "1/2 cup heavy cream",
+          "Salt and pepper to taste"
+        ]
+      })
+    );
+  });
+
+  it('Nothing found using filter parameter should return an empty array', async () => {
+    const { client } = setUp();
+
+    const response = await client.get('/recipes?filter=DAJIDjadiaj');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.items).toEqual([]);
+  });
 });
 
 function setUp() {
