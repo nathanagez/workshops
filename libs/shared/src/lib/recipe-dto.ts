ad4017e9aed1d54a82cc8b2f335b2c49f2076905
Nathan Agez <nathan.agez@gmail.com>
2023-12-07 15:46:52 +0100

    feat(recipe-dto): create RecipeDto & RecipeResponseDto

--- libs/shared/src/lib/recipe-dto.ts
+++ libs/shared/src/lib/recipe-dto.ts
@@ -1,8 +1,12 @@
-export interface Recipe {
+export interface RecipeDto {
   id: string;
   name: string;
   description: string;
   pictureUrl: string;
   steps: Array<string>;
   ingredients: Array<string>
+}
+
+export interface RecipeResponseDto {
+  items: RecipeDto[];
 }
